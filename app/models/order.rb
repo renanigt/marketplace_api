5751b60fca55b041b428e33855fd7861620cb705
renanigt <renanigt@gmail.com>
2020-10-24 00:36:10 -0300

    creates callback method to calculate Order's total value

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,15 @@
 class Order < ApplicationRecord
+  before_validation :set_total!
+
   belongs_to :user
   has_many :placements
   has_many :products, through: :placements
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  private
+
+  def set_total!
+    self.total = self.products.sum(&:price)
+  end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,29 +3,24 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   setup do
     @user = users(:one)
+    @products = [products(:two), products(:three)]
   end
 
   test "valid" do
-    order = Order.new(user: @user, total: 20)
+    order = Order.new(user: @user, total: 20, products: @products)
 
     assert order.valid?
   end
 
   test "invalid without user" do
-    order = Order.new(total: 20)
+    order = Order.new(products: @products)
 
     assert_not order.valid?
   end
 
-  test "invalid without total" do
-    order = Order.new(user: @user)
+  test "set_total!" do
+    order = Order.create!(user: @user, products: @products)
 
-    assert_not order.valid?
-  end
-
-  test "invalid with negative total" do
-    order = Order.new(user: @user, total: -1)
-
-    assert_not order.valid?
+    assert_equal @products.sum(&:price), order.total
   end
 end
